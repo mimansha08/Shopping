d07ea28e3e62c77a7480e88380c2c1f522f8c150
Abhinav Kumar <abhinav@avitechlab.com>
2023-02-11 01:09:38 +0530

    improve build script

--- init.js
+++ init.js
@@ -1,4 +1,5 @@
 const fs= require("fs");
 const path=require("path")
 const data= fs.readFileSync(path.join(__dirname,"server/sample-db.json"));
-fs.writeFileSync(path.join(__dirname,"server/db.json"),data);
+if ( !fs.existsSync(path.join(__dirname, "server/db.json")))
+	fs.writeFileSync(path.join(__dirname,"server/db.json"),data);
